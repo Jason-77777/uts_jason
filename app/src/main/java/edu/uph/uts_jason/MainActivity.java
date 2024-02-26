9960d79fe31396954cb37e75627e55eace5650a5
Jason-77777 <jason.fan2828@gmail.com>
2024-02-26 20:01:24 +0700

    a

--- app/src/main/java/edu/uph/uts_jason/MainActivity.java
+++ app/src/main/java/edu/uph/uts_jason/MainActivity.java
@@ -1,8 +1,11 @@
 package edu.uph.uts_jason;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find the verelangkasa LinearLayout by its ID
+        LinearLayout verelangkasaLayout = findViewById(R.id.verelangkasa);
+
+        // Set a click listener for the verelangkasa LinearLayout
+        verelangkasaLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create an intent to start MainActivity2
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+
+                // Start MainActivity2
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/edu/uph/uts_jason/MainActivity2.java
@@ -0,0 +1,14 @@
+package edu.uph.uts_jason;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
