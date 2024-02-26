59e20b13d0e4e2c535d9ac2b836b5a449ad2c609
Jason-77777 <jason.fan2828@gmail.com>
2024-02-26 20:51:22 +0700

    a

--- app/src/main/java/edu/uph/uts_jason/MainActivity3.java
+++ app/src/main/java/edu/uph/uts_jason/MainActivity3.java
@@ -1,10 +1,11 @@
 package edu.uph.uts_jason;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,14 +16,29 @@ public class MainActivity3 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        LinearLayout ferelLayout = findViewById(R.id.ferel);
 
-        ferelLayout.setOnClickListener(new View.OnClickListener() {
+        LinearLayout ferelaaaLayout = findViewById(R.id.ferelaaa);
+
+
+        ferelaaaLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity3.this, MainActivity.class);
 
-                startActivity(intent);
+                EditText studentIdEditText = findViewById(R.id.editTextText);
+                EditText namaEditText = findViewById(R.id.editTextText2);
+                EditText jurusanEditText = findViewById(R.id.editTextText3);
+                EditText tahunMasukEditText = findViewById(R.id.editTextText4);
+
+                String studentId = studentIdEditText.getText().toString();
+                String nama = namaEditText.getText().toString();
+                String jurusan = jurusanEditText.getText().toString();
+                String tahunMasuk = tahunMasukEditText.getText().toString();
+
+
+                String toastMessage = "Student ID: " + studentId + "\nNama: " + nama +
+                        "\nJurusan: " + jurusan + "\nTahun Masuk: " + tahunMasuk;
+
+                Toast.makeText(MainActivity3.this, toastMessage, Toast.LENGTH_SHORT).show();
             }
         });
     }
